b502476983b24a9c47186290ffb3590df1fb9b7e
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2021-03-31 12:31:33 +0200

    Flere nodes

--- src/com/p4/core/nodes/IDNode.java
+++ src/com/p4/core/nodes/IDNode.java
@@ -2,11 +2,9 @@ package com.p4.core.nodes;
 
 import com.p4.core.visitors.INodeVisitor;
 
-public class IntNode extends DclNode<Integer>{
-    public IntNode(String id) {
-        super(id);
-    }
+public class IDNode extends AstNode {
 
+    @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
     }
