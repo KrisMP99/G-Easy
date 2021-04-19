7478d4100905b193fcc388a74682f787ca61e993
Cecilie Fog <cfog19@student.aau.dk>
2021-04-19 13:04:29 +0200

    Added symboltable cause i'm so great, but not really

--- src/com/p4/core/symbolTable/SymbolTable.java
+++ src/com/p4/core/symbolTable/SymbolTable.java
@@ -1,9 +1,7 @@
 package com.p4.core.symbolTable;
 
-import com.sun.source.tree.Scope;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Stack;
 
 public class SymbolTable {
@@ -18,15 +16,100 @@ public class SymbolTable {
         currentScope = new Scope("local");
     }
 
-    public void addScope() {}
+    public Scope getCurrentScope() {
+        return currentScope;
+    }
+
+    //Add a scope to the symbol table
+    public void addScope(String scopeName) {
+        //Enters, if the scope has not already been entered
+        if (lookupScope(scopeName) == null) {
+            Scope scope = new Scope(scopeName);
+            scope.setParentScope(currentScope);
+            currentScope.addScopeChild(scope);
+            scopeStack.push(currentScope);
+            currentScope = scope;
+        }
+    }
 
     public void leaveScope() {
         this.leaveScope(null);
     }
 
-    private void leaveScope(String scopeName) {
-        if (currentScope.get)
+    public void leaveScope(String scopeName) {
+        //Enters if we're in a non-global scope
+        if (currentScope.getParentScope() != null){
+            if (scopeName != null){
+                currentScope.setScopeName(scopeName);
+            }
+
+            //Sets the current scope to the outer scope and removes the scope from the stack
+            currentScope = scopeStack.empty() ? globalScope : scopeStack.pop();
+        }
     }
 
+    public void enterScope(String scopeName) {
+        Scope scope = this.findScope(scopeName, globalScope);
 
+        if (scope != null){
+            scopeStack.push(currentScope):
+            currentScope = scope;
+        }
+    }
+
+    private Scope findScope(String scopeName, Scope currentScope) {
+        //Check if the scope we are looking for, is the current scope
+        if (currentScope.getScopeName().equals(scopeName)){
+            return currentScope;
+        }
+
+        Scope scope = null;
+
+        //Iterates through all the nested scopes
+        for (Scope childScope : currentScope.getScopeChildren()) {
+            scope = this.findScope(scopeName, childScope);
+
+            //Enter if the scope we're looking for is found
+            if (scope != null){
+                break;
+            }
+        }
+
+        //If none was found, returns null
+        return scope;
+    }
+
+    public Scope lookupScope(String scopeName) {
+        return this.findScope(scopeName, globalScope);
+    }
+
+    public SymbolAttribute lookupSymbol(String symbol){
+        Scope scope = currentScope;
+
+        do {
+            //Is entered, if the symbol is a parameter
+            if (!scope.getParams().isEmpty() && scope.getParams().containsKey(symbol)) {
+                return scope.getParams().get(symbol);
+            }
+
+            //Is entered if the symbol is a regular symbol AND is found in the current scope
+            if (!scope.getSymbols().isEmpty() && scope.getSymbols().containsKey(symbol)) {
+                return scope.getSymbols().get(symbol);
+            }
+        } while((scope = scope.getParentScope()) != null);
+
+        return null;
+    }
+
+    public void insertSymbol(String symbol, SymbolAttribute attribute) {
+        currentScope.getSymbols().put(symbol, attribute);
+    }
+
+    public void insertParam(String paramId, SymbolAttribute attribute) {
+        currentScope.getParams().put(paramId, attribute);
+    }
+
+    public boolean isFunctionsAndLoopDefined() {
+        return (declaredFunctions.contains("function") && declaredFunctions.contains("loop"));
+    }
 }
