7d1c34a2ce8ac2c4ae3b495bb4ccde248290f071
KrisMP99 <kpede19@student.aau.dk>
2021-04-22 11:43:12 +0200

    Implemented INodeVisitor

---
+++ src/com/p4/core/visitors/SemanticsVisitor.java
@@ -0,0 +1,170 @@
+package com.p4.core.visitors;
+
+import com.p4.core.nodes.*;
+
+public class SemanticsVisitor implements INodeVisitor {
+    @Override
+    public void visitChildren(AstNode node) {
+
+    }
+
+    @Override
+    public void visit(ProgNode node) {
+
+    }
+
+    @Override
+    public void visit(DclNode node) {
+
+    }
+
+    @Override
+    public void visit(FuncDclNode node) {
+
+    }
+
+    @Override
+    public void visit(FuncCallNode node) {
+
+    }
+
+    @Override
+    public void visit(AssignNode node) {
+
+    }
+
+    @Override
+    public void visit(ArrayDclNode node) {
+
+    }
+
+    @Override
+    public void visit(PosDclNode node) {
+
+    }
+
+    @Override
+    public void visit(VarDclNode node) {
+
+    }
+
+    @Override
+    public void visit(PosAssignNode node) {
+
+    }
+
+    @Override
+    public void visit(ArrayAccessNode node) {
+
+    }
+
+    @Override
+    public void visit(ExprNode node) {
+
+    }
+
+    @Override
+    public void visit(SelectionNode node) {
+
+    }
+
+    @Override
+    public void visit(IterativeNode node) {
+
+    }
+
+    @Override
+    public void visit(LogicalExprNode node) {
+
+    }
+
+    @Override
+    public void visit(FormalParamNode node) {
+
+    }
+
+    @Override
+    public void visit(ActualParamNode node) {
+
+    }
+
+    @Override
+    public void visit(BlockNode node) {
+
+    }
+
+    @Override
+    public void visit(ReturnExprNode node) {
+
+    }
+
+    @Override
+    public void visit(ArithmeticNode node) {
+
+    }
+
+    @Override
+    public void visit(CompNode node) {
+
+    }
+
+    @Override
+    public void visit(BoolDclNode node) {
+
+    }
+
+    @Override
+    public void visit(BoolExprNode node) {
+
+    }
+
+    @Override
+    public void visit(CompExprNode node) {
+
+    }
+
+    @Override
+    public void visit(IDNode node) {
+
+    }
+
+    @Override
+    public void visit(BoolNode node) {
+
+    }
+
+    @Override
+    public void visit(IntDclNode node) {
+
+    }
+
+    @Override
+    public void visit(IntNode node) {
+
+    }
+
+    @Override
+    public void visit(DoubleDclNode node) {
+
+    }
+
+    @Override
+    public void visit(DoubleNode node) {
+
+    }
+
+    @Override
+    public void visit(PosNode node) {
+
+    }
+
+    @Override
+    public void visit(LogicalOPNode node) {
+
+    }
+
+    @Override
+    public void visit(LineCommentNode node) {
+
+    }
+}
